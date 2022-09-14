8afc6a5b0288712ddf1088c202271b05216164e0
AbaidooPrince <abaidooprince@gmail.com>
2022-09-14 05:34:08 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
-#include<stdio.h>
-/*
- *main - entry point
- *@void: no argument
- *Return: Always 0(success)
- */
+#include "main.h"
+
+/**
+*  main - Entry point
+*
+* Return: returns 0 on success
+*/
+
 int main(void)
 {
-printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
