e6b8b51d7dc07d5ac969c93a232572117fb3e29a
AbaidooPrince <abaidooprince@gmail.com>
2022-09-15 18:10:47 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+ * main- test that the function positive_negative returns 0
+ * Return: 0
+*/
+
+
+int main (void)
+{
+	int i;
+
+	i = 0;	
+	positive_or_negative(i);
+
+	return (0);
+}
+
